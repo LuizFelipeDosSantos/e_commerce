f934ec27e79da2ab6eefb7e85afcf5e482af03da
Luiz Felipe dos Santos <luizfelipesantos.sc@gmail.com>
2022-05-10 16:55:31 +0200

    Solved Address routes

--- e_commerce_user/routes/address.routes.js
+++ e_commerce_user/routes/address.routes.js
@@ -5,19 +5,19 @@ const User = require("../models/User.model");
 
 const isLoggedIn = require("../middleware/isLoggedIn");
 
-router.get("/address/list", isLoggedIn, (req, res) => {
+router.get("/list", isLoggedIn, (req, res) => {
   res.json(req.user);
 });
 
-router.post("/address/create", isLoggedIn, (req, res) => {
+router.post("/create", isLoggedIn, (req, res) => {
 
 });
 
-router.put("/address/edit", isLoggedIn, (req, res) => {
+router.put("/edit", isLoggedIn, (req, res) => {
 
 });
 
-router.delete("/address/delete", isLoggedIn, (req, res) => {
+router.delete("/delete", isLoggedIn, (req, res) => {
 
 });
 
