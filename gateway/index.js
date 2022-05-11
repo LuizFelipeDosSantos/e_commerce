9bae3447993bf966250c72ec84287fcac704aaa1
Luiz Felipe dos Santos <luizfelipesantos.sc@gmail.com>
2022-05-11 16:06:29 +0200

    Created shopping route

--- gateway/index.js
+++ gateway/index.js
@@ -7,9 +7,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/user', proxy('http://localhost:5006'))
-app.use('/products', proxy('http://localhost:5007'))
-
+app.use('/user', proxy('http://localhost:5006'));
+app.use('/product', proxy('http://localhost:5007'));
+app.use('/shopping', proxy('http://localhost:5008'));
 
 app.listen(5005, () => {
     console.log('Gateway is Listening to Port 5005')
